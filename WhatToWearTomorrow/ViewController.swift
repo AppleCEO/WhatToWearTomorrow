fa74d828d037020b4f49d67e19c2c57e60fc1b8c
도미닉 <kiljh@me.com>
2025-09-01 15:31:07 +0900

    프로젝트 생성

---
+++ WhatToWearTomorrow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToWearTomorrow
+//
+//  Created by 도미닉 on 9/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
