c70bc666cb99ccaa3b33c3ffa74a3071f200913c
도미닉 <kiljh@me.com>
2025-09-12 19:27:51 +0900

    ViewController 이름 변경

--- WhatToWearTomorrow/OnboardViewController.swift
+++ WhatToWearTomorrow/OnboardViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OnboardViewController.swift
 //  WhatToWearTomorrow
 //
 //  Created by 도미닉 on 9/1/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
